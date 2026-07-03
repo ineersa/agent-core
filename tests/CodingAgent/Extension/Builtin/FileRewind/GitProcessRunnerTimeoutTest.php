4d5fb4827f565495302f2b74e6049fe1dfd4301a
Illia Vasylevskyi <ineersa@gmail.com>
2026-07-03 14:11:50 -0400

    test(file-rewind): fix duplicate FileRewindLedgerStoreLockTest class in GitProcessRunnerTimeoutTest

--- tests/CodingAgent/Extension/Builtin/FileRewind/GitProcessRunnerTimeoutTest.php
+++ tests/CodingAgent/Extension/Builtin/FileRewind/GitProcessRunnerTimeoutTest.php
@@ -4,40 +4,15 @@ declare(strict_types=1);
 
 namespace Ineersa\CodingAgent\Tests\Extension\Builtin\FileRewind;
 
-use Ineersa\CodingAgent\Extension\Builtin\FileRewind\FileRewindLedgerStore;
-use Ineersa\CodingAgent\Extension\Builtin\FileRewind\RewindProjectIdentity;
+use Ineersa\CodingAgent\Extension\Builtin\FileRewind\GitProcessRunner;
 use Ineersa\CodingAgent\Tests\Support\TestDirectoryIsolation;
 use PHPUnit\Framework\TestCase;
 
-final class FileRewindLedgerStoreLockTest extends TestCase
+final class GitProcessRunnerTimeoutTest extends TestCase
 {
-    private string $projectDir;
-
-    protected function setUp(): void
-    {
-        $this->projectDir = TestDirectoryIsolation::createProjectTempDir('file-rewind-ledger-lock');
-    }
-
-    protected function tearDown(): void
-    {
-        TestDirectoryIsolation::removeDirectory($this->projectDir);
-    }
-
-    public function testSequentialAppendCheckpointPersistsToLedger(): void
-    {
-        $identity = RewindProjectIdentity::fromProjectRoot($this->projectDir);
-        $store = new FileRewindLedgerStore($this->projectDir);
-        $store->appendCheckpoint($identity, ['run_id' => 'r1', 'turn_no' => 1]);
-        $store->appendCheckpoint($identity, ['run_id' => 'r1', 'turn_no' => 2]);
-        $rows = $store->readCheckpoints($identity);
-        self::assertCount(2, $rows);
-        self::assertSame(1, $rows[0]['turn_no']);
-        self::assertSame(2, $rows[1]['turn_no']);
-    }
-
     public function testRunTerminatesHungGitInvocationAfterTimeout(): void
     {
-        $binDir = \Ineersa\CodingAgent\Tests\Support\TestDirectoryIsolation::createOsTempDir('fake-git-bin');
+        $binDir = TestDirectoryIsolation::createOsTempDir('fake-git-bin');
         $gitScript = $binDir.'/git';
         $script = <<<'SH'
 #!/bin/sh
@@ -61,8 +36,7 @@ SH;
             self::assertLessThan(5.0, $elapsed, 'Hung git should be terminated near configured timeout');
         } finally {
             putenv('PATH='.$oldPath);
-            \Ineersa\CodingAgent\Tests\Support\TestDirectoryIsolation::removeDirectory($binDir);
+            TestDirectoryIsolation::removeDirectory($binDir);
         }
     }
-
 }
